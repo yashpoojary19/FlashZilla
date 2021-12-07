b895f034bb6eb5aa8216a2711ae4199e5218206c
Yash Poojary <yashspoojary245@gmail.com>
2021-12-07 16:17:55 +0530

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Yash Poojary on 07/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
