06c27b7ca207ab6e080a5b4f0d9856a3213baa7f
Yash Poojary <yashspoojary245@gmail.com>
2021-12-08 12:20:24 +0530

    Upto haptics

--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -6,16 +6,181 @@
 //
 
 import SwiftUI
+import CoreHaptics
+
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("World")
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            print("VStack Tapped")
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+//
+//struct ContentView: View {
+//    var body: some View {
+//        ZStack {
+//            Rectangle()
+//                .fill(Color.blue)
+//                .frame(width: 300, height: 300)
+//                .onTapGesture {
+//                    print("Rectangle Tapped")
+//                }
+//
+//            Circle()
+//                .fill(Color.red)
+//                .frame(width: 300, height: 300)
+//                .contentShape(Rectangle())
+//                .onTapGesture {
+//                    print("Circle Tapped")
+//                }
+//
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//
+//    @State private var engine: CHHapticEngine?
+//
+//
+//    var body: some View {
+//        Text("Hello World!")
+//            .onAppear(perform: prepareHaptics)
+//            .onTapGesture(perform: complexSuccess)
+//    }
+//
+//    func simpleSuccess() {
+//        let generator = UINotificationFeedbackGenerator()
+//        generator.notificationOccurred(.success)
+//    }
+//
+//    func prepareHaptics() {
+//        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+//
+//        do {
+//
+//            engine = try CHHapticEngine()
+//            try engine?.start()
+//
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+//    }
+//
+//    func complexSuccess() {
+//
+//        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+//
+//        var events = [CHHapticEvent]()
+//
+//
+//        for i in stride(from: 0, to: 1, by: 0.1) {
+//            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+//            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+//
+//            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+//            events.append(event)
+//        }
+//
+//
+//        for i in stride(from: 0, to: 1, by: 0.1) {
+//            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+//            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+//
+//            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+//            events.append(event)
+//        }
+//
+//
+//
+//
+//
+//
+//
+//        do {
+//
+//            let pattern = try CHHapticPattern(events: events, parameters: [])
+//            let player = try engine?.makePlayer(with: pattern)
+//            try player?.start(atTime: 0)
+//
+//        } catch {
+//            print("Failed to play pattern: \(error.localizedDescription)")
+//        }
+//
+//
+//
+//    }
+//}
+// struct ContentView: View {
+//
+//    @State private var offset = CGSize.zero
+//    @State private var isDragging = false
+//
+//
+//    var body: some View {
+//
+//        let dragGesture = DragGesture().onChanged { value in
+//            offset = value.translation
+//        }
+//            .onEnded { _ in
+//                withAnimation {
+//                    offset = .zero
+//                    isDragging = false
+//                }
+//            }
+//
+//        let pressGesture = LongPressGesture().onEnded { _ in
+//            withAnimation {
+//                isDragging = true
+//            }
+//
+//        }
+//
+//        let combined = pressGesture.sequenced(before: dragGesture)
+//
+//
+//        return Circle()
+//            .fill(Color.red)
+//            .frame(width: 64, height: 64)
+//            .scaleEffect(isDragging ? 1.5 : 1)
+//            .offset(offset)
+//            .gesture(combined)
+//
+//    }
+//}
+
+// struct ContentView: View {
+//
+//    var body: some View {
+//        VStack {
+//            Text("Hello World")
+//                .onTapGesture {
+//                    print("Text Tapped")
+//                }
+//        }
+//        .simultaneousGesture(
+//            TapGesture()
+//                .onEnded { _ in
+//                    print("VStack Tapped")
+//                }
+//
+//        )
+//
+//    }
+//}
